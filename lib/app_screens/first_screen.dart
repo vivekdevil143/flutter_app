b032c889b86152957344875c5a8177639fa072bd
Vivekrai123 <vivekdevil143@gmail.com>
2019-10-07 00:08:26 +0530

    Second commit 06/10/2019

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 class FirstScreen extends StatelessWidget{
   @override
@@ -6,12 +8,19 @@ class FirstScreen extends StatelessWidget{
       color: Colors.lightBlueAccent,
       child: Center(
         child: Text(
-          "Hello Flutter",
+          generateLuckyNumber(),
           textDirection: TextDirection.ltr,
           style: TextStyle(color: Colors.white, fontSize: 40.0),
         ),
       ),
     );
+  }
+
+  String generateLuckyNumber(){
+
+    var random=Random();
+    int luckynumber=random.nextInt(10);
+    return "Your Lucky Number is $luckynumber";
   }
 
 }
